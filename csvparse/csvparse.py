b0fe00fc74a3dd4438f658997a014048c0a2ef59
Zac Hamid <zac.hamid@uk.ibm.com>
2016-10-07 16:01:40 +0100

    Debug info added to index.html

--- csvparse/csvparse.py
+++ csvparse/csvparse.py
@@ -1,5 +1,6 @@
-import os,csv
+import os,csv,sys
 from django.conf import settings
+from django import template
 
 class CSVFile:
 
@@ -14,19 +15,23 @@ class CSVFile:
 		for i in range(len(self.data[0])):
 			field = self.data[:][i]
 			types = [0]*len(field)
+
 			for j in range(len(field)):
-				types[j] = type(field[j])
-			for j in range(len(field)):
-				if types[j] == 'str':
-					self.valid[j]+=1
+				print >>sys.stderr,type(field[j])
+				if type(field[j]) is str:
+					self.valid[i]+=1
 				else:
-					self.valid[j]-=1
-			if self.valid[j] < 0:
-				self.type[j] = 'Numeric'
+					self.valid[i]-=1
+
+				self.data[i][j] = str(self.data[i][j]) + '(' + str(type(self.data[i][j])) + ')'
+
+			if self.valid[i] > 0:
+				self.type[i] = 'Numeric'
 			else:
-				self.type[j] = 'Alpha'
+				self.type[i] = 'Alpha'
+
 		for i in range(len(self.data[0])):
-			self.valid[i] = abs(self.valid[i])/len(self.data[0])
+			self.valid[i] = self.valid[i]/len(self.data[0])
 
 def csvparse():
 	files = []
@@ -39,3 +44,9 @@ def csvparse():
 		file_obj = CSVFile(filename, data)
 		files.append(file_obj)
 	return files
+
+register = template.Library()
+
+@register.filter
+def get_type(value):
+    return type(value)
